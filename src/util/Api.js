a20b548801a9b17f71f3dfb814cf2f8ce53310c7
linear-digital <tamizuddin.tr@gmail.com>
2024-02-03 14:55:33 +0600

    update Appointment.jsx and Api.js

--- src/util/Api.js
+++ src/util/Api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:4000/api/"
+    // baseURL: "http://localhost:4000/api/"
+    baseURL: 'https://service-center-backend.vercel.app/api/'
 })
 export default api
